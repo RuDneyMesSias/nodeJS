c2b9da49d3a1467af98cc66f96f5dd50c87b013c
Rudney Messias <rudney_vinicius@hotmail.com>
2023-10-13 20:35:44 -0300

    views_ejs

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const app = express();
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-//Rotas
+app.set('view engine','ejs');
 
-app.get('/',(req, res) =>{
-    res.send('HEllo WOrd THIAGUINHO gay!!!!');
+
+//Rota pontando para pasta views.
+app.get("/",(req, res) =>{
+    res.render("principal/perfil");
 });
+
 
 app.listen(3000, function(erro){
     if(erro){
-        console.log("Ocorreu um erro!");
+        console.log("Ocorreu um erro! ${port}");
     }else{
         console.log("Servidor iniciado com sucesso!");
     }
