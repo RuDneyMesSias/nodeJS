91942ee737cc6a084f348b382b1dc8ff8f8072db
Rudney Messias <rudney_vinicius@hotmail.com>
2023-10-14 11:22:15 -0300

    Metodos EJS

--- index.js
+++ index.js
@@ -6,23 +6,37 @@ const HOST = '0.0.0.0';
 
 app.set('view engine','ejs');
 
-
 //Rota pontando para pasta views.
-app.get("/",(req, res) =>{
-    var nome = "Rudney Messias ";
-    var lang = "JavaScript"
-    res.render("index", {
+
+app.get("/:nome/:lang",(req,res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-cola", preco: 5},
+        {nome: "leite", preco: 3.14},
+        {nome: "Carne", preco: 15},
+        {nome: "REdBull", preco: 10},
+        {nome: "NEscal", preco: 6.50}
+    ]
+
+
+    res.render("index",{
         nome: nome,
-        lang:lang,
+        lang: lang,
         empresa: "Guia do programador",
-        inscritos: 8000
+        inscritos: 5400,
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
-
 app.listen(3000, function(erro){
     if(erro){
-        console.log("Ocorreu um erro! ${port}");
+        console.log("Ocorreu um erro!");
     }else{
         console.log("Servidor iniciado com sucesso!");
     }
