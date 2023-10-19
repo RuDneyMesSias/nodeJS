8a896d884118b4a772c823033caa3b1fd39302ed
Rudney Messias <rudney_vinicius@hotmail.com>
2023-10-18 21:18:06 -0300

    partials

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express'); //Importando o express
 const app = express();
+const bodyParser =require("body-parser"); //Traduzir dados do formulário para JavaScript
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+// Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Rota pontando para pasta views.
 
@@ -15,6 +21,12 @@ app.get("/",(req,res) => {
 
 app.get("/perguntar",(req,res)=> {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req,res)=> {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
 });
 
 app.listen(3000, function(erro){
